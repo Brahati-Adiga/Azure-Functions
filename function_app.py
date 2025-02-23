2ac28532bc4443e9816b470e448f43bb7a2b13c8
Brahati Adiga <brahati2002@gmail.com>
2025-02-23 17:05:31 +0530

    modified return

--- function_app.py
+++ function_app.py
@@ -24,6 +24,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. The value of {c} ",
+             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. The value of c is {c} ",
              status_code=200
         )
